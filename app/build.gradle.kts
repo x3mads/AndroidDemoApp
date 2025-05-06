508491bfbda34602cdc399fe6f1cf21d205e344e
German Gonzalez <german.gonzalez@x3mads.com>
2025-05-06 16:44:11 -0300

    Updated to latest adapters versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.etermax.xmediator.test_app"
+        applicationId = "com.x3mads.xmediator.demo"
         minSdk = 23
         targetSdk = 34
         versionCode = 1
@@ -54,9 +54,7 @@ dependencies {
     implementation("com.x3mads.android.xmediator:core:1.104.1")
     implementation("com.x3mads.android.xmediator.mediation:renderer:2.1.1.6.0")
     implementation("com.x3mads.android.xmediator.mediation:applovin:13.2.0.1")
-    implementation("com.x3mads.android.xmediator.mediation:google-ads:24.2.0.0")
-    implementation("com.x3mads.android.xmediator.mediation:ironsource:8.7.0.0")
-    implementation("com.x3mads.android.xmediator.mediation:fairbid:3.60.0.0")
+    implementation("com.x3mads.android.xmediator.mediation:ironsource:8.8.0.0")
 
     // -- Ad Networks --
     // amazon publisher services
@@ -67,76 +65,65 @@ dependencies {
     implementation("com.x3mads.android.xmediator.mediation:aps:10.0.0.0")
     // applovin
     implementation("com.applovin:applovin-sdk:13.2.0")
-    implementation("com.google.ads.mediation:applovin:13.2.0.1")
-    implementation("com.ironsource.adapters:applovinadapter:4.3.53")
+    implementation("com.unity3d.ads-mediation:applovin-adapter:4.3.53")
     implementation("com.x3mads.android.xmediator.mediation:applovin:13.2.0.1")
     // chartboost
     implementation("com.chartboost:chartboost-sdk:9.8.3")
-    implementation("com.google.ads.mediation:chartboost:9.8.3.1")
-    implementation("com.ironsource.adapters:chartboostadapter:4.3.19")
+    implementation("com.unity3d.ads-mediation:chartboost-adapter:4.3.19")
     implementation("com.applovin.mediation:chartboost-adapter:9.8.3.0")
     implementation("com.x3mads.android.xmediator.mediation:chartboost:9.8.3.0")
     // meta audience network
     implementation("com.facebook.android:audience-network-sdk:6.19.0")
-    implementation("com.google.ads.mediation:facebook:6.19.0.1")
-    implementation("com.ironsource.adapters:facebookadapter:4.3.49")
+    implementation("com.unity3d.ads-mediation:facebook-adapter:4.3.49")
     implementation("com.applovin.mediation:facebook-adapter:6.19.0.2")
     implementation("com.x3mads.android.xmediator.mediation:facebook:6.19.0.0")
     // dt exchange
     implementation("com.fyber:marketplace-sdk:8.3.7")
-    implementation("com.google.ads.mediation:fyber:8.3.7.0")
-    implementation("com.ironsource.adapters:fyberadapter:4.3.38")
+    implementation("com.unity3d.ads-mediation:fyber-adapter:4.3.38")
     implementation("com.applovin.mediation:fyber-adapter:8.3.7.0")
     implementation("com.x3mads.android.xmediator.mediation:fyber:8.3.7.0")
     // google ads
     implementation("com.google.android.gms:play-services-ads:24.2.0")
-    implementation("com.ironsource.adapters:admobadapter:4.3.50")
+    implementation("com.unity3d.ads-mediation:admob-adapter:4.3.50")
     implementation("com.applovin.mediation:google-ad-manager-adapter:24.2.0.0")
     implementation("com.applovin.mediation:google-adapter:24.2.0.1")
     implementation("com.x3mads.android.xmediator.mediation:google-ads:24.2.0.0")
     // hyprmx
     implementation("com.hyprmx.android:HyprMX-SDK:6.4.2")
-    implementation("com.hyprmx.android:HyprMX-AdMob:6.4.2.2")
-    implementation("com.ironsource.adapters:hyprmxadapter:4.3.12")
+    implementation("com.unity3d.ads-mediation:hyprmx-adapter:4.3.12")
     implementation("com.applovin.mediation:hyprmx-adapter:6.4.2.3")
     implementation("com.x3mads.android.xmediator.mediation:hyprmx:6.4.2.0")
     // inmobi
     implementation("com.inmobi.monetization:inmobi-ads-kotlin:10.8.2")
-    implementation("com.google.ads.mediation:inmobi:10.8.2.0")
-    implementation("com.ironsource.adapters:inmobiadapter:4.3.29")
+    implementation("com.unity3d.ads-mediation:inmobi-adapter:4.3.29")
     implementation("com.applovin.mediation:inmobi-adapter:10.8.2.0")
     implementation("com.x3mads.android.xmediator.mediation:inmobi:10.8.0.0")
     // ironsource
-    implementation("com.ironsource.sdk:mediationsdk:8.7.0")
-    implementation("com.google.ads.mediation:ironsource:8.7.0.1")
-    implementation("com.applovin.mediation:ironsource-adapter:8.7.0.0.0")
-    implementation("com.x3mads.android.xmediator.mediation:ironsource:8.7.0.0")
+    implementation("com.unity3d.ads-mediation:mediation-sdk:8.8.0")
+    implementation("com.applovin.mediation:ironsource-adapter:8.8.0.0.1")
+    implementation("com.x3mads.android.xmediator.mediation:ironsource:8.8.0.0")
     // mintegral
     implementation("com.mbridge.msdk.oversea:mbridge_android_sdk:16.9.61")
-    implementation("com.google.ads.mediation:mintegral:16.9.71.0")
-    implementation("com.ironsource.adapters:mintegraladapter:4.3.40")
+    implementation("com.unity3d.ads-mediation:mintegral-adapter:4.3.40")
     implementation("com.applovin.mediation:mintegral-adapter:16.9.71.0")
     implementation("com.x3mads.android.xmediator.mediation:mintegral:16.9.61.0")
     // mobilefuse
     implementation("com.mobilefuse.sdk:mobilefuse-sdk-core:1.9.0")
-    implementation("com.mobilefuse.sdk:mobilefuse-adapter-admob:1.9.0.0")
     implementation("com.unity3d.ads-mediation:mobilefuse-adapter:4.3.5")
     implementation("com.applovin.mediation:mobilefuse-adapter:1.9.0.0")
     implementation("com.x3mads.android.xmediator.mediation:mobile-fuse:1.9.0.0")
     // moloco
     implementation("com.moloco.sdk.adapters:adapter:3.9.0")
-    implementation("com.google.ads.mediation:moloco:3.9.0.0")
     implementation("com.unity3d.ads-mediation:moloco-adapter:4.3.17")
     implementation("com.applovin.mediation:moloco-adapter:3.9.0.0")
     // ogury
     implementation("co.ogury:ogury-sdk:6.0.1")
-    implementation("com.ironsource.adapters:oguryadapter:4.3.2")
+    implementation("com.unity3d.ads-mediation:ogury-adapter:4.3.2")
     implementation("com.applovin.mediation:ogury-presage-adapter:6.0.1.1")
     implementation("com.x3mads.android.xmediator.mediation:ogury:6.0.1.0")
     // pangle
     implementation("com.pangle.global:ads-sdk:6.5.0.9")
-    implementation("com.google.ads.mediation:pangle:6.5.0.9.0")
-    implementation("com.ironsource.adapters:pangleadapter:4.3.38")
+    implementation("com.unity3d.ads-mediation:pangle-adapter:4.3.38")
     implementation("com.applovin.mediation:bytedance-adapter:6.5.0.8.1")
     implementation("com.x3mads.android.xmediator.mediation:pangle:6.5.0.8.0")
     // bidmachine
@@ -149,14 +136,12 @@ dependencies {
     implementation("com.x3mads.android.xmediator.mediation:tappx:4.1.11.0")
     // unity ads
     implementation("com.unity3d.ads:unity-ads:4.14.2")
-    implementation("com.google.ads.mediation:unity:4.14.2.0")
-    implementation("com.ironsource.adapters:unityadsadapter:4.3.54")
+    implementation("com.unity3d.ads-mediation:unityads-adapter:4.3.54")
     implementation("com.applovin.mediation:unityads-adapter:4.14.2.0")
     implementation("com.x3mads.android.xmediator.mediation:unity:4.14.2.0")
     // liftoff monetize
     implementation("com.vungle:vungle-ads:7.4.3")
-    implementation("com.google.ads.mediation:vungle:7.4.3.1")
-    implementation("com.ironsource.adapters:vungleadapter:4.3.29")
+    implementation("com.unity3d.ads-mediation:vungle-adapter:4.3.29")
     implementation("com.applovin.mediation:vungle-adapter:7.4.3.2")
     implementation("com.x3mads.android.xmediator.mediation:vungle:7.4.3.0")
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,13 +9,13 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven { url = uri("https://android-artifact-registry.x3mads.com/maven") }
-        maven { url = uri("https://android-sdk.is.com") }
         maven { url = uri("https://artifact.bytedance.com/repository/pangle") }
         maven { url = uri("https://artifactory.bidmachine.io/bidmachine") }
         maven { url = uri("https://cboost.jfrog.io/artifactory/chartboost-ads") }
         maven { url = uri("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea") }
-        maven { url = uri("https://maven.ogury.co") }
         google()
+        maven { url = uri("https://maven.ogury.co") }
+        maven { url = uri("https://repo.maven.apache.org/maven2") }
         mavenCentral()
     }
 }
