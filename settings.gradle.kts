b58c8fbd6d7b52e4968aa410faadd1653e9d927b
German Gonzalez <german.gonzalez@x3mads.com>
2025-08-14 07:42:21 -0300

    Updated to latest adapters versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven { url = uri("https://android-artifact-registry.x3mads.com/maven") }
+        maven { url = uri("https://android-sdk.is.com") }
         maven { url = uri("https://artifact.bytedance.com/repository/pangle") }
         maven { url = uri("https://artifactory.bidmachine.io/bidmachine") }
         maven { url = uri("https://cboost.jfrog.io/artifactory/chartboost-ads") }
